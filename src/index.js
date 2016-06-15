dc710ff26bb73f8f3ac9bba090b4dd8fe35ca796
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-15 15:44:15 +0200

    Refactor App into Class component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 //Path reference to own files. Not needed for libraries.
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCzPG9rlPb_UdgU0BHK6H5S-lqS3mdtQas';
 
-//Create new HTML creating component. App is top level, with child components within.
-const App = () => {
-	//JSX, subset of JS that looks like JS
-	return ( 
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'puppies'}, function(data) {
+		console.log(data);
+		});
+	}
+
+	render () {
+		return ( 
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 // Insert new HTML component into DOM.
