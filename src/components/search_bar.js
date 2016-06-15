a3d30b279cb36d1d78d890b94298ef8a34aad46a
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-15 14:07:07 +0200

    Set state of search term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 
 //Defines new class and give it access to al components React has.
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	//Render and returning JSX is necessary with React.Component
 	render() {
-		return <input />;
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value})} />
+			</div>
+		);
 	}
 }
 
-//Functional component - output of a function. Cannot do much with
-//user inout, unlike class components. This is less intelligent.
-// const SearchBar = () => {
-// 	return <input />;
-// };
-
-//Any file can now import and read SearchBar
 export default SearchBar;
