c6fb7d431642d322588731a9b3ede032b848d0c6
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-15 11:57:47 +0200

    Print app constant to the DOM with React and ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //Create new HTML creating component.
 const App = function() {
 	//JSX, subset of JS that looks like JS
 	return <div>Hi!</div>;
 }
 
-//Insert new HTML component into DOM.
-
+// Insert new HTML component into DOM.
+//Instantiate App, and second argument tells where to instantiate it
+ReactDOM.render(<App />, document.querySelector('.container'));
