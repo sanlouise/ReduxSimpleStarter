2640b520b299ceb23f1c27ea11bbd7db1263434c
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-15 18:18:00 +0200

    Add unique ID to all video list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import VideoListItem from './video_list_item'
 
+//Props comes from index.html, <VideoList videos={this.state.videos}/> What is passed through, is called a 'prop'
+//Video.etag is a unique identifier that comes with the Youtube API. All list elements need a unique ID.
 const VideoList = (props) => {
+	const videoItems = props.videos.map((video) => {
+		return <VideoListItem key ={video.etag} video={video} />
+	});
+
 
 	return (
 		//className instead of class to avoid naming conflicts
 		<ul className="col-md-4 list-group">
-		{props.videos.length}
+			{videoItems}
 		</ul>
 
 	);
